23973aa27d852391a7b0e73a4a10cf4549698990
imbhascker13 <58689890+imbhascker13@users.noreply.github.com>
2020-04-22 09:17:37 +0530

    Update first_deployment.tf

--- 04-Terraform/02-First_Deployment/first_deployment.tf
+++ 04-Terraform/02-First_Deployment/first_deployment.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = "AKIAYKBCL35Z4D6JMLFR"
+  access_key = "AKIAYKBCL35Z4D6JMLFS"
   secret_key = "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"
   region     = "us-west-2"
 }
